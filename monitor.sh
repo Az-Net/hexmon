10b3a1dd5144a89993009ffa159c06820fd61699
Atmatan Kabbaher <119499525+Az-Neter@users.noreply.github.com>
2024-07-13 11:33:55 -0400

    Update monitor.sh
    Color start

--- monitor.sh
+++ monitor.sh
@@ -1,6 +1,32 @@
 #!/bin/bash
-While :; do
-sleep 0.5
+
+colorize_hex() {
+  local hex=$1
+  case "$hex" in
+    0FF0) echo -e "\e[31m$hex\e[0m";;  # Red
+    F0F0) echo -e "\e[32m$hex\e[0m";;  # Green
+
+    *) echo "$hex";; # Default
+  esac
+}
+
+while :; do
+sleep 0.25
+clear
 git add . && git commit -m .
-tail -c 325 hexmon
+echo " "
+tail -c 325 hexmon | while read -r line_num hex1 hex2 hex3 hex4 hex5 hex6 hex7 hex8 ascii; do
+echo -n "$line_num" 
+echo -n "$(colorize_hex "$hex1") "
+echo -n "$(colorize_hex "$hex2") "
+echo -n "$(colorize_hex "$hex3") "
+echo -n "$(colorize_hex "$hex4") "
+echo -n "$(colorize_hex "$hex5") "
+echo -n "$(colorize_hex "$hex6") "
+echo -n "$(colorize_hex "$hex7") "
+echo -n "$(colorize_hex "$hex8") "
+echo -n "$ascii"
+echo " "
+done
+echo " "
 done
